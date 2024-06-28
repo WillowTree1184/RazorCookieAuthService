f6e080a6d645100ecbd7ab8fe15d3f1b86ab9fc4
WillowTree1184 <114594480+WillowTree1184@users.noreply.github.com>
2024-06-28 22:25:52 +0800

    Add files via upload

---
+++ CookieAuthService/AuthController.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace CookieAuthService
+{
+    /// <summary>
+    /// A controller to write and delete cookie
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public readonly IHttpContextAccessor httpContextAccessor;
+        static string indexPageURL;
+        static string loginPageURL;
+        static KeyValuePair<string, string> queryWhenTimeOut;
+        static KeyValuePair<string, string> queryWhenTokenIdError;
+
+        /// <summary>
+        /// Initialize AuthController
+        /// </summary>
+        /// <param name="indexPageURL">The url which will be redirect when login successful</param>
+        /// <param name="loginPageURL">The url which will be redirect when login unsuccessful or logout successful</param>
+        /// <param name="queryWhenTimeOut">The query which will been added when login time out</param>
+        /// <param name="queryWhenTokenIdError">The query which will been added if the tokenId is unregisted when login</param>
+        public static void Initialize(string indexPageURL, string loginPageURL, KeyValuePair<string, string> queryWhenTimeOut, KeyValuePair<string, string> queryWhenTokenIdError)
+        {
+            AuthController.indexPageURL = indexPageURL;
+            AuthController.loginPageURL = loginPageURL;
+            AuthController.queryWhenTimeOut = queryWhenTimeOut;
+            AuthController.queryWhenTokenIdError = queryWhenTokenIdError;
+        }
+
+        public AuthController(IHttpContextAccessor httpContextAccessor)
+        {
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        /// <summary>
+        /// Check the checksum and write the token to the cookie.
+        /// </summary>
+        /// <param name="token">The token</param>
+        /// <param name="checksum">The checksum</param>
+        /// <returns>
+        /// When check success：Redirect to indexPageURL
+        /// When check fail：Redirect to loginPageURL
+        /// </returns>
+        [Route("login")]
+        [HttpGet]
+        public async Task<ActionResult>? Login(string tokenId, string checksum)
+        {
+            if (!ChecksumService.Check(checksum))
+            {
+                return Redirect($"{loginPageURL}?{queryWhenTimeOut.Key}={queryWhenTimeOut.Value}");
+            }
+
+            string token;
+            if (!TokenDictionaryService.TryPopToken(tokenId, out token))
+            {
+                return Redirect($"{loginPageURL}?{queryWhenTokenIdError.Key}={queryWhenTokenIdError.Value}");
+            }
+            Console.WriteLine(token);
+            
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim("token", token)
+            };
+
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            AuthenticationProperties authProperties = new AuthenticationProperties
+            {
+                ExpiresUtc = DateTime.UtcNow.AddMonths(1)
+            };
+
+            await httpContextAccessor.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProperties);
+
+            return Redirect(indexPageURL);
+        }
+
+        /// <summary>
+        /// Delete cookie
+        /// </summary>
+        /// <returns>Redirect to loginPageURL</returns>
+        [Route("logout")]
+        [HttpGet]
+        public async Task<ActionResult> Logout()
+        {
+            await httpContextAccessor.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return Redirect(loginPageURL);
+        }
+    }
+}
---
+++ CookieAuthService/ChecksumService.cs
@@ -0,0 +1,50 @@
+﻿namespace CookieAuthService
+{
+    /// <summary>
+    /// A service to manage the checksum
+    /// </summary>
+    public static class ChecksumService
+    {
+        static string nowChecksum;
+        static string lastChecksum;
+        public static string checksum => nowChecksum;
+
+        static System.Timers.Timer? updateTimer;
+        /// <summary>
+        /// Occurs when the checksum updated
+        /// </summary>
+        public static event EventHandler<string>? ChecksumUpdated;
+
+        /// <summary>
+        /// Initialize the checksum calculator
+        /// </summary>
+        /// <param name="updateInterval">Checksum update interval</param>
+        public static void Initialize(int updateInterval)
+        {
+            UpdateChecksum();
+            lastChecksum = nowChecksum;     //It is the first time to generate the checksum, so the lastChecksum must equals the nowChecksum
+
+            updateTimer = new System.Timers.Timer(updateInterval);
+            updateTimer.Elapsed += (source, e) => UpdateChecksum();
+            updateTimer.AutoReset = true;
+            updateTimer.Enabled = true;
+        }
+
+        /// <summary>
+        /// Generate a new checksum
+        /// </summary>
+        static void UpdateChecksum()
+        {
+            lastChecksum = nowChecksum;
+            nowChecksum = Sha256Calculator.GetSha256Value($"{Guid.NewGuid()}Checksum{Random.Shared.Next()}{Random.Shared.Next()}{Random.Shared.Next()}{Random.Shared.Next()}{Random.Shared.Next()}{Random.Shared.Next()}{DateTime.Now.Ticks}");   //Use complex and unique strings to generate checksums
+            ChecksumUpdated?.Invoke(lastChecksum, nowChecksum);
+        }
+
+        /// <summary>
+        /// Check the checksum
+        /// </summary>
+        /// <param name="checksum">The checksum</param>
+        /// <returns>When passed, return true; Otherwise, it returns false</returns>
+        public static bool Check(string checksum) => checksum == nowChecksum || checksum == lastChecksum;
+    }
+}
